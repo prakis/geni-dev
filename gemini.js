767472a30a7fe342b2e38b4e4a04d0b808f6cac8
Krishna Pagadala <prakis@gmail.com>
2025-01-06 15:49:18 +0530

    trimmer and code clean added

--- gemini.js
+++ gemini.js
@@ -3,6 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const MD_CODE_QUOTES = '```';
 
 function testcall(){
     return 'success';
@@ -12,7 +13,21 @@ async function askGemini(question){
     const result = await model.generateContent(question);
     const text_restult = result.response.text();
     //console.log(text_restult);
-    return text_restult;
+    return clearResult(text_restult);
+}
+function clearResult(result){
+    let trimmed = result.trim();
+    if(trimmed.length <= 6){
+        return trimmed;
+    }
+    let isStartsWithCodeQuotes = trimmed.startsWith(MD_CODE_QUOTES);
+    let isEndsWithCodeQuotes = trimmed.endsWith(MD_CODE_QUOTES);
+    let cleanedResult = "";
+    if(isStartsWithCodeQuotes && isEndsWithCodeQuotes){
+        cleanedResult = trimmed.substr(3, trimmed.length-6);
+    }
+
+    return cleanedResult.trim();
 }
 
 module.exports = { testcall, askGemini }
